a9b384000df4f38c99964f747a3bce5864404958
Christa Caitlyn <140872769+ccrepez@users.noreply.github.com>
2026-09-20 10:49:45 +0700

    books

---
+++ view_updateBook.php
@@ -0,0 +1,64 @@
+<?php
+
+require("controller_book.php");
+
+if (isset($_GET["updateID"])) {
+    $book_id = $_GET["updateID"];
+    $book = getBookID($book_id);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Book</title>
+</head>
+
+<style>
+* {
+    margin: 0;
+    box-sizing: border-box;
+}
+</style>
+
+<body style="font-family: Arial, sans-serif;">
+    <nav
+        style="width: 100%; background-color: #f5f5f5; padding: 25px 40px; display: flex; justify-content: space-between; align-items: center; box-shadow: 0 2px 8px rgba(0,0,0,0.15);">
+        <h2 style="color: black;">Library Management</h2>
+        <div>
+            <a href="#" style="color: black; text-decoration: none; margin-right: 35px;">Members</a>
+            <a href="#" style="color: black; text-decoration: none; margin-right: 35px;">Books</a>
+            <a href="#" style="color: black; text-decoration: none;">Borrowings</a>
+        </div>
+    </nav>
+
+    <div style="padding: 35px 40px;">
+        <h2 style="margin-bottom: 20px;">Update Book</h2>
+        <form method="POST" action="controller_book.php">
+            <label>Title</label><br>
+            <input type="text" name="title" placeholder="Enter title" value="<?= $book->getTitle() ?>"
+                style="width: 100%; padding: 12px; margin: 8px 0 18px; border: 1px solid #ccc; border-radius: 6px;">
+
+            <label>Author</label><br>
+            <input type="text" name="author" placeholder="Enter author" value="<?= $book->getAuthor() ?>"
+                style="width: 100%; padding: 12px; margin: 8px 0 18px; border: 1px solid #ccc; border-radius: 6px;">
+
+            <label>Publisher</label><br>
+            <input type="text" name="publisher" placeholder="Enter publisher" value="<?= $book->getPublisher() ?>"
+                style="width: 100%; padding: 12px; margin: 8px 0 18px; border: 1px solid #ccc; border-radius: 6px;">
+
+            <label>Genre</label><br>
+            <input type="text" name="genre" placeholder="Enter genre" value="<?= $book->getGenre() ?> "
+                style="width: 100%; padding: 12px; margin: 8px 0 25px; border: 1px solid #ccc; border-radius: 6px;">
+
+            <input type="hidden" name="id" value="<?= $book_id ?>">
+            <button name="update" type="submit"
+                style="width: 100%; padding: 12px; background-color: #212529; color: white; border: none; border-radius: 6px;">Update
+                Book</button>
+        </form>
+    </div>
+</body>
+
+</html>
